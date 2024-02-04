4fbac5bf09314563fd3c66697d1a3cded9464b41
Asad Moinuddin <113580280+AsadMoinuddin152@users.noreply.github.com>
2024-02-04 16:20:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,42 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import path from "path";
+import { fileURLToPath } from "url"; // Added import
 
-//Configure ENV
+// Configure ENV
 dotenv.config();
 
-//DATABASE CONNECTION
+// DATABASE CONNECTION
 connectDB();
 
-//REST OBJECT
+// REST OBJECT
 const app = express();
 
-//MIDDLEWARE
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Getting the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-//ROUTES
+// ROUTES
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//REST API
-app.get("*", function (req, res) {
+// REST API
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 const PORT = process.env.PORT || 8080;
 
-//PORT
+// PORT
 app.listen(PORT, () => {
   console.log(
     `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}.`
